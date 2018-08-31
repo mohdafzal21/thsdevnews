3133f197edb02f388527fe3dc5da842180fa9b91
mohammed afzal <mohammedafzal94@gmail.com>
2018-09-01 00:25:36 +0530

    added meta tags

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const indexRouter = require('./routes/index')
-const userRouter = require('./routes/user');
+
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use('/public',express.static(path.join(__dirname, '/public')));
 
 
 app.use('/', indexRouter);
-app.use('/user', userRouter);
+
 
 
 
