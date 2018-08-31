229a5df7f7fcefb4dd2458c2e563145297427021
mohammed afzal <mohammedafzal94@gmail.com>
2018-08-31 17:31:04 +0530

    added basic routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const GoogleNewsRss = require('google-news-rss');
- 
 const googleNews = new GoogleNewsRss();
  
 
+router.get('/',(req,res)=>{
+    res.render('index')
+});
+
+router.get('/react',(req,res)=>{
+    res.render('react')
+});
 
 router.get('/api/react', (req,res)=>{
     googleNews
    .search('react js')
    .then(resp => res.json(resp));
 })
-
 
 module.exports = router;
